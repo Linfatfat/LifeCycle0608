20defe51b58f49cb9a8679e16e4bccc122ba9175
Linfatfat <s940110118@gmail.com>
2020-06-08 11:09:13 +0800

    布局完成

--- app/src/main/java/com/u1065115/lifecycle0608/MainActivity.java
+++ app/src/main/java/com/u1065115/lifecycle0608/MainActivity.java
@@ -2,6 +2,9 @@ package com.u1065115.lifecycle0608;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find   R   id
+        Button btnDial = findViewById(R.id.btnDial);
+        Button btnSecond = findViewById(R.id.btnSecond);
+        Button btnFinish = findViewById(R.id.btnFinish);
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
+
+        //設定 button   click   事件共用的傾聽    myListener
+        btnDial.setOnClickListener(myListener);
     }
+    private  Button.OnClickListener myListener= new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
---
+++ app/src/main/java/com/u1065115/lifecycle0608/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.u1065115.lifecycle0608;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
