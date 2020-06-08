ddab1946fe2bc01cab6568c06b9caf72f5386646
Linfatfat <s940110118@gmail.com>
2020-06-08 11:50:06 +0800

    完成專案

--- app/src/main/java/com/u1065115/lifecycle0608/MainActivity.java
+++ app/src/main/java/com/u1065115/lifecycle0608/MainActivity.java
@@ -1,10 +1,14 @@
 package com.u1065115.lifecycle0608;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +22,73 @@ public class MainActivity extends AppCompatActivity {
         Button btnSecond = findViewById(R.id.btnSecond);
         Button btnFinish = findViewById(R.id.btnFinish);
 
+        //設定 button   click   事件共用的傾聽    myListener
+        btnDial.setOnClickListener(myListener);
+        btnSecond.setOnClickListener(myListener);
+        btnFinish.setOnClickListener(myListener);
+
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
 
-        //設定 button   click   事件共用的傾聽    myListener
-        btnDial.setOnClickListener(myListener);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart(1)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume(1)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart(1)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, " onPause(1)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, " onStop(1)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, " onDestroy(1)", Toast.LENGTH_LONG).show();
+    }
+
     private  Button.OnClickListener myListener= new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnDial:
+                    Uri uri= Uri.parse("tel:099123456");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+                    break;
+
+                case  R.id.btnSecond:
+                     Intent intent1 = new Intent(MainActivity.this,SecondActivity.class);
+                     startActivity(intent1);
+                     break;
+
+                case  R.id.btnFinish:
+                    finish();
+                    break;
+
+            }
 
         }
     };
